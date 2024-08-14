9fee0b8ee29e144a5888e30df26d08def5cc4d9a
李博帅 <109013521+liboshuai01@users.noreply.github.com>
2024-08-14 19:57:28 +0800

    添加mybatis组件包

---
+++ slr-framework/slr-spring-boot-starter-mybatis/src/main/java/com/liboshuai/starlink/slr/framework/translate/config/SlrTranslateAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.liboshuai.starlink.slr.framework.translate.config;
+
+import com.fhs.trans.service.impl.TransService;
+import com.liboshuai.starlink.slr.framework.translate.core.TranslateUtils;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+public class SlrTranslateAutoConfiguration {
+
+    @Bean
+    @SuppressWarnings({"InstantiationOfUtilityClass", "SpringJavaInjectionPointsAutowiringInspection"})
+    public TranslateUtils translateUtils(TransService transService) {
+        TranslateUtils.init(transService);
+        return new TranslateUtils();
+    }
+
+}
