8cdddc08662ad26f83465b6e2efa34443c7fe171
李博帅 <109013521+liboshuai01@users.noreply.github.com>
2024-08-18 02:32:12 +0800

    错误码常量 代码优化

--- slr-connector/slr-connector-api/src/main/java/com/liboshuai/starlink/slr/connector/api/constants/ErrorCodeConstants.java
+++ slr-connector/slr-connector-api/src/main/java/com/liboshuai/starlink/slr/connector/api/constants/ErrorCodeConstants.java
@@ -4,13 +4,11 @@ package com.liboshuai.starlink.slr.connector.api.constants;
 import com.liboshuai.starlink.slr.framework.common.exception.ErrorCode;
 
 /**
- * connector 错误码枚举类
- *
- * connector 系统，使用 10001-20000 段
+ * connector 错误码常量
+ * {connector 系统，使用 10001-20000 段}
  */
 public interface ErrorCodeConstants {
 
-    // 严重问题，完全驳回，不会上送任何数据
     ErrorCode UPLOAD_EVENT_ERROR = new ErrorCode(10001, "上送事件数据错误");
 
 }
